34008487999063c06f4ed3a02df21160beac0412
Niroshan Jayathilaka <niroshanjwm@gmail.com>
2019-04-04 10:18:36 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+const kerberos = require('kerberos');
+const hostname = process.env.KERBEROS_HOSTNAME || 'hostname.example.com';
+
+
+// In this function we will get the principal detail from Kerberos server
+// Please refer: https://www.npmjs.com/package/kerberos#principalDetails
+
+kerberos.principalDetails('HTTP', hostname, (err, details) => {
+    console.log(err, details);
+});
