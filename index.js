1a10bea89c9d479f3e568897306d7dfebe7363ae
Niroshan Jayathilaka <niroshanjwm@gmail.com>
2019-04-04 12:16:13 +0530

    add package json

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const hostname = process.env.KERBEROS_HOSTNAME || 'hostname.example.com';
 
 // In this function we will get the principal detail from Kerberos server
 // Please refer: https://www.npmjs.com/package/kerberos#principalDetails
-
 kerberos.principalDetails('HTTP', hostname, (err, details) => {
     console.log(err, details);
 });
